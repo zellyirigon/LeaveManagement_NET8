9b850f4ca2e803bff238fe973a65cb921c596e3a
Zelly Irigon <zellyirigon@hotmail.com>
2024-01-30 15:10:11 +1300

    Added Database Tables and Migrations. End Implementation Database Section.

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Primitives;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? FirstName {  get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? TaxId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateJoined { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays {  get; set; }
+
+
+        [ForeignKey("LeaveTypeId")] // data annotation
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId {  get; set; }
+
+
+        public string EmployeeId {  get; set; }
+
+    }
+}
