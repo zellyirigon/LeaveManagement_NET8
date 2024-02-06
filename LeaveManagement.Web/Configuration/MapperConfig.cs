b239e84e89084bdfaeb71b00e780f4d2fd82e289
Zelly Irigon <zellyirigon@hotmail.com>
2024-02-06 22:47:04 +1300

    Modified the CRUD screens and added databases and swal. End of Leave Type Management Module.

---
+++ LeaveManagement.Web/Configuration/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+using Microsoft.Build.Framework.Profiler;
+
+namespace LeaveManagement.Web.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
